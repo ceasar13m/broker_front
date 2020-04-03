1034af66a087d933d8168c71f4ae9f818b5bf0b1
ainur <muharlyamovy@gmail.com>
2020-04-03 19:49:38 +0300

    0.0.1

---
+++ src/components/auth/sign_in/sign_in.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import styles from "./sign_in.module.css";
+
+class SignIn extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return (
+            <div className={styles.signInPage}>
+
+            </div>
+        )
+    }
+
+}
